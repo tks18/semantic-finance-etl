9c11732ead68a771ca23b06db24f81d62dda97e1
tks18 <tksudharshan@gmail.com>
2026-07-16 15:48:26 +0530

    test(tests/config): add proper pytest cases for config based operations

--- tests/cases/config/test_config_service.py
+++
@@ -1,6 +0,0 @@
-from semantic_finance_etl.config.services.project_config_service import (
-    ProjectConfigService,
-)
-
-config = ProjectConfigService().load("tests/samples/configs")
-print(config.model_dump())
