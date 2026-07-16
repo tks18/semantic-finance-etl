75b97f020737a6bd93095d01b3cf19d2d86a8899
tks18 <tksudharshan@gmail.com>
2026-07-16 15:49:49 +0530

    test(tests/etl): add domain and validation service related tests

--- tests/cases/hooks/test_hooks_import.py
+++
@@ -1,5 +0,0 @@
-from semantic_finance_etl.contracts.hook import TableHook
-from semantic_finance_etl.domain.models.hook_payloads import BatchPayload
-from semantic_finance_etl.domain.models.hook_results import HookExecutionResult
-
-print("Hook contract imports working.")
